f8a2bb538876de8ddba10d395b150ddadc453c8a
Gregory Price <gregory.price@memverge.com>
2023-12-01 23:05:02 +0000

    mm/mempolicy: allow home_node to be set by mpol_new
    This patch adds the plumbing into mpol_new() to allow the argument
    structure's home_node field to be set during mempolicy creation.
    
    The syscall sys_set_mempolicy_home_node was added to allow a home
    node to be registered for a vma.
    
    For set_mempolicy2 and mbind2 syscalls, it would be useful to add
    this as an extension to allow the user to submit a fully formed
    mempolicy configuration in a single call, rather than require
    multiple calls to configure a mempolicy.
    
    This will become particularly useful if/when pidfd interfaces to
    change process mempolicies from outside the task appear, as each
    call to change the mempolicy does an atomic swap of that policy
    in the task, rather than mutate the policy. The change in this
    patch will prevent the need to do that atomic swap multiple times.
    
    Signed-off-by: Gregory Price <gregory.price@memverge.com>

--- mm/mempolicy.c
+++ mm/mempolicy.c
@@ -335,7 +335,7 @@ static struct mempolicy *mpol_new(struct mempolicy_param *param)
 	atomic_set(&policy->refcnt, 1);
 	policy->mode = mode;
 	policy->flags = flags;
-	policy->home_node = NUMA_NO_NODE;
+	policy->home_node = param->home_node;
 	policy->wil.cur_weight = 0;
 
 	return policy;
@@ -1648,6 +1648,7 @@ static long kernel_set_mempolicy(int mode, const unsigned long __user *nmask,
 	param.mode = lmode;
 	param.mode_flags = mode_flags;
 	param.policy_nodes = &nodes;
+	param.home_node = NUMA_NO_NODE;
 
 	return do_set_mempolicy(&param);
 }
@@ -3025,6 +3026,8 @@ void mpol_shared_policy_init(struct shared_policy *sp, struct mempolicy *mpol)
 		mparam.mode = mpol->mode;
 		mparam.mode_flags = mpol->flags;
 		mparam.policy_nodes = &mpol->w.user_nodemask;
+		mparam.home_node = NUMA_NO_NODE;
+
 		/* contextualize the tmpfs mount point mempolicy to this file */
 		npol = mpol_new(&mparam);
 		if (IS_ERR(npol))
@@ -3183,6 +3186,7 @@ void __init numa_policy_init(void)
 	memset(&param, 0, sizeof(param));
 	param.mode = MPOL_INTERLEAVE;
 	param.policy_nodes = &interleave_nodes;
+	param.home_node = NUMA_NO_NODE;
 
 	if (do_set_mempolicy(&param))
 		pr_err("%s: interleaving failed\n", __func__);
@@ -3197,6 +3201,7 @@ void numa_default_policy(void)
 
 	memset(&param, 0, sizeof(param));
 	param.mode = MPOL_DEFAULT;
+	param.home_node = NUMA_NO_NODE;
 
 	do_set_mempolicy(&param);
 }
@@ -3319,6 +3324,8 @@ int mpol_parse_str(char *str, struct mempolicy **mpol)
 	mparam.mode = mode;
 	mparam.mode_flags = mode_flags;
 	mparam.policy_nodes = &nodes;
+	mparam.home_node = NUMA_NO_NODE;
+
 	new = mpol_new(&mparam);
 	if (IS_ERR(new))
 		goto out;
